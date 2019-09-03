19418d77461d8ce204db07f89c8e79ff40f243f3
enNaDev <45121983+enNaDev@users.noreply.github.com>
2019-09-03 16:05:30 +0300

    Update app.js

--- app.js
+++ app.js
@@ -50,6 +50,7 @@ app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
 
-app.listen(process.env.PORT, process.env.IP, function(){
-   console.log("The YelpCamp Server Has Started!");
+var port = process.env.PORT || 3000;
+app.listen(port, function(){
+  console.log("Server Has Started!");
 });
